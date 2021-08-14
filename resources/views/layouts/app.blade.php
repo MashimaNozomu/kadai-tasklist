b4ac3adc50ddab5d96980a00f973429f1769acd9
nozomu.mashima <mashimawork6@gmail.com>
2021-08-14 05:54:21 +0000

    kadai-tasklist

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         </header>
         
          {{-- ナビゲーションバー --}}
-        @include('commons.navbar')
+        
         <div class="container">
             {{-- エラーメッセージ --}}
             @include('commons.error_messages')
